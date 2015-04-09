124a1da7bb504570443cdb270874e6d641ab7124
unknown <G00247578@gmit.ie>
2015-04-09 12:15:50 +0100

    morecleanup

--- www/js/app.js
+++ www/js/app.js
@@ -1,5 +1,3 @@
-// Ionic Starter App
-
 angular.module('cavamenu', ['ionic', 'cavamenu.controllers'])
 
 .config(function($stateProvider, $urlRouterProvider) {
@@ -13,41 +11,15 @@ angular.module('cavamenu', ['ionic', 'cavamenu.controllers'])
   })
 
   .state('app.dishes', {
-    url: "/search",
+    url: "/dishes/:dishesId",
     views: {
       'menuContent': {
-        templateUrl: "templates/search.html"
-      }
-    }
-  })
-
-  .state('app.browse', {
-    url: "/browse",
-    views: {
-      'menuContent': {
-        templateUrl: "templates/browse.html"
-      }
-    }
-  })
-    .state('app.playlists', {
-      url: "/playlists",
-      views: {
-        'menuContent': {
-          templateUrl: "templates/playlists.html",
-          controller: 'PlaylistsCtrl'
-        }
-      }
-    })
-
-  .state('app.single', {
-    url: "/playlists/:playlistId",
-    views: {
-      'menuContent': {
-        templateUrl: "templates/playlist.html",
+        
+        templateUrl: "templates/dishes.html",
         controller: 'PlaylistCtrl'
       }
     }
   });
   // if none of the above states are matched, use this as the fallback
-  $urlRouterProvider.otherwise('/app/playlists');
+  $urlRouterProvider.otherwise('/app/dishes/1');
 });
